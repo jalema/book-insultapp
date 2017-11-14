19d42ff8cfe7e089b3c720fa68bcd5d2bdecfdde
jalema <33656417+jalema@users.noreply.github.com>
2017-11-14 13:36:50 +0100

    Update InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -4,14 +4,14 @@ import java.util.Random;
 
 public class InsultGenerator {
 	public String generateInsult() {
-		String words[][] = {{"madafacka", "mamon", "juanjo"}, {"fistro", "pecador", "juanjo"}, {"empollon", "cuatrojos", "juanjo"}};
+		String words[][] = {{"madafacka", "mamon", "empollon"}, {"fistro", "pecador", "cuatrojos"}, {"juanjo", "juanjo", "juanjo"}};
 		String vowels = "AEIOU";
 		String article = "un";
 		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
 		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
 		String noun = words[2][new Random().nextInt(words[2].length)];
 		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
-			article = "a";
+			article = "una";
 		}
 		return String.format("Eres %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
 	}
